6ac72e18c4f3c1db1c3a3506edde1ee04c04adf7
s33ding <robertomdiniz@protonmail.com>
2025-06-08 23:01:21 -0300

    sv

--- src/up.sh
+++ src/up.sh
@@ -1,4 +1,5 @@
-aws s3 cp . s3://dataiesb/ --recursive 
+#aws s3 cp . s3://dataiesb/ --recursive 
 #aws s3 cp ocorrencias/main.py s3://dataiesb/ocorrencias/ 
-#aws s3 cp  index.html s3://dataiesb/
+aws s3 cp  s3://dataiesb/reports.json .
+aws s3 cp  . s3://dataiesb/
 
